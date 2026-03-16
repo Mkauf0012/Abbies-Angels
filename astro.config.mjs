df7e7f8a6147e3ab7c2881efbc5c1df0579ae76d
AAKRON <ITDEPARTMENT@AAKRONLINE.COM>
2026-03-16 11:14:39 -0400

    Allow Netlify Visual Editor devserver host in Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ export default defineConfig({
   output: 'static',
   build: {
     assets: 'assets'
+  },
+  vite: {
+    server: {
+      allowedHosts: [
+        'devserver-feature-astro-conversion--dancing-treacle-6631ef.netlify.app',
+        '.netlify.app'
+      ]
+    }
   }
 });
