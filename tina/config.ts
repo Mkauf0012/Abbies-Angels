bd33e7bfad6a67a8647c790b5cb370e7b1af9330
Mkauf0012 <Matthew.Kaufman@outlook.com>
2026-03-16 16:10:15 -0400

    Hardcode branch name for Tina Cloud

--- tina/config.ts
+++ tina/config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "tinacms";
 
-const branch =
-  process.env.CF_PAGES_BRANCH ||
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
-
 export default defineConfig({
-  branch,
+  branch: "feature/astro-conversion",
 
   clientId: "f59c2a31-2913-4e06-a5f1-36ad54eb6fc4",
   token: "13b83d4477d0ce33a0661d692976e1f1803c01cf",
