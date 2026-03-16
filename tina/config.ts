d524c11e1d8aac5d5a594eb7fc1c3ab945cebd7b
Mkauf0012 <Matthew.Kaufman@outlook.com>
2026-03-16 16:06:15 -0400

    Fix branch detection for Cloudflare Pages

--- tina/config.ts
+++ tina/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tinacms";
 
 const branch =
+  process.env.CF_PAGES_BRANCH ||
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
