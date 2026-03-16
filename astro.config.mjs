3b6c1d1e14eeb82894123d14a6758d857082a1ab
AAKRON <ITDEPARTMENT@AAKRONLINE.COM>
2026-03-16 10:14:18 -0400

    Remove Netlify adapter from astro.config - not needed for static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'static',
-  adapter: netlify(),
   build: {
     assets: 'assets'
   }
